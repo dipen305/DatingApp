94db4a2b7a57bc5979bb73a9a499fc3a1186ea65
dipen305 <hdipen@gmail.com>
2020-03-17 10:10:02 +0530

    Update

--- DatingApp.API/Data/Datacontext.cs
+++ DatingApp.API/Data/Datacontext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
     {
         public Datacontext(DbContextOptions options) : base(options) {}
             public DbSet<Value> Values { get; set; }
+            public DbSet<User> Users { get; set; }
     }
 }
